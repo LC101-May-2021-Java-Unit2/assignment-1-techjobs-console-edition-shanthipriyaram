e92c503f4032fc5d85ae9e8df5ce3e282355ace6
shanthipriyaram <shanthipriyamba@gmail.com>
2021-09-22 13:16:48 -0500

    Changes made

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -77,9 +74,9 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
+            String aValue = row.get(column).toLowerCase();
 
-            if (aValue.contains(value)) {
+            if (aValue.contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -99,8 +96,19 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
-    }
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        for (HashMap<String, String> row : allJobs) {
+            for (Map.Entry<String, String> col : row.entrySet()) {
+                String aValue = col.getValue().toLowerCase();
+                if (aValue.contains(value)) {
+                    jobs.add(row);
+                }
+            }
+        }
+            return jobs;
+        }
+        //return null;
+
 
     /**
      * Read in data from a CSV file and store it in a list
